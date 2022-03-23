aff423e1ad302ecfe20d8f281db5201060c5542f
zedmous <ejnsilva29@gmail.com>
2022-03-23 04:15:23 -0400

    implementando table ordenamiento y busqueda por filtro de angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { ListaEmpleadosComponent } from './pages/empleados/lista-empleados/lista-empleados.component';
 import { FormEmpleadosComponent } from './pages/empleados/form-empleados/form-empleados.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { FormEmpleadosComponent } from './pages/empleados/form-empleados/form-em
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    AppRoutingModule,
     SharedModule
   ],
   providers: [],
